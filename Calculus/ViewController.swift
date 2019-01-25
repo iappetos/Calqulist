b68d66a09da048c4737e25354b0c98315ea427a4
Ioannis Karagogos <ikaragogos@gmail.com>
2019-01-25 14:12:13 +0200

    Initial Commit

---
+++ Calculus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculus
+//
+//  Created by planB on 25/1/19.
+//  Copyright © 2019 Ioannis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
